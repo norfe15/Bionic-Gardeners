e638752d879ec1f216130038b0dc9bb493160e8e
NorthPew <vbergstrom02@gmail.com>
2023-04-05 11:52:26 +0200

    Tested a different method but same result

--- src/components/ToTopBtn.jsx
+++ src/components/ToTopBtn.jsx
@@ -11,7 +11,8 @@ function ToTopBtn() {
     }
 
     const top = () => {
-        window.scrollTo(0, 0)
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
     }
 
     return (
