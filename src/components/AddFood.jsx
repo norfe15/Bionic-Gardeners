539d7fe8053151275f8439996fa896feb1a4db2c
Willi4mL <wi.lindbolm@gmail.com>
2023-04-12 23:02:50 +0200

    validation add name, ingredient and price done

--- src/components/AddFood.jsx
+++ src/components/AddFood.jsx
@@ -1,13 +1,23 @@
 import { useState, useEffect } from "react"
 import { foodMenuArray } from "./foodMenuArray"
+import { isValid, isValidIngredient, isValidPrice } from "./validation"
 
 function AddFood() {
 	const [array, setArray] = useState([...foodMenuArray])
-
 	const [name, setName] = useState("")
 	const [ingredients, setIngredients] = useState("")
 	const [img, setImg] = useState("")
 	const [price, setPrice] = useState("")
+	const [wrongOrderName, setWrongOrderName] = useState(false)
+	const [wrongOrderIngredient, setWrongOrderIngredient] = useState(false)
+	const [wrongOrderPrice, setWrongOrderPrice] = useState(false)
+
+	const [isValidOrderName, notValidOrderName] = isValid(name)
+	const isValidClassOrder = wrongOrderName ? (isValidOrderName ? 'valid' : 'invalid') : ''
+	const [isValidOrderIngredient, notValidOrderIngredient] = isValidIngredient(ingredients)
+	const isValidClassIngredient = wrongOrderIngredient ? (isValidOrderIngredient ? 'valid' : 'invalid') : ''
+	const [isValidOrderPrice, notValidOrderPrice] = isValidPrice(price)
+	const isValidClassPrice = wrongOrderPrice ? (isValidOrderPrice ? 'valid' : 'invalid') : ''
 
 	const handleNameChange = (e) => {
 		setName(e.target.value)
@@ -50,29 +60,47 @@ function AddFood() {
 			<h1 className="add-food-header">Lägg till ny maträtt</h1>
 			<div className="add-food">
 				<form className="add-food-form" onSubmit={handleSubmit}>
-					<input
-						placeholder="Namn"
-						id="add-food-input-name"
-						type="text"
-						value={name}
-						onChange={handleNameChange}
-					/>
+					<div className="order-container">
+						<input
+							placeholder="Namn"
+							id="add-food-input-name"
+							type="text"
+							value={name}
+							className={isValidClassOrder}
+							onChange={handleNameChange}
+							onBlur={() => setWrongOrderName(true)}>
+						</input>
+						<span className='input-icon-order'>{wrongOrderName ? (isValidOrderName ? '✔️' : '❌') : ''}</span>
+						<span className='display-error-order'> {wrongOrderName ? notValidOrderName : ''}</span>
+					</div>
 
-					<input
-						placeholder="Ingredienser"
-						id="add-food-input-ingredients"
-						type="text"
-						value={ingredients}
-						onChange={handleIngredientsChange}
-					/>
+					<div className="order-container">
+						<input
+							placeholder="Ingredienser"
+							id="add-food-input-ingredients"
+							type="text"
+							value={ingredients}
+							className={isValidClassIngredient}
+							onChange={handleIngredientsChange}
+							onBlur={() => setWrongOrderIngredient(true)}>
+						</input>
+						<span className='input-icon-order'>{wrongOrderIngredient ? (isValidOrderIngredient ? '✔️' : '❌') : ''}</span>
+						<span className='display-error-order'> {wrongOrderIngredient ? notValidOrderIngredient : ''}</span>
+					</div>
 
-					<input
-						placeholder="Pris"
-						id="add-food-input-price"
-						type="text"
-						value={price}
-						onChange={handlePriceChange}
-					/>
+					<div className="order-container">
+						<input
+							placeholder="Pris"
+							id="add-food-input-price"
+							type="text"
+							value={price}
+							className={isValidClassPrice}
+							onChange={handlePriceChange}
+							onBlur={() => setWrongOrderPrice(true)}>
+						</input>
+						<span className='input-icon-order'>{wrongOrderPrice ? (isValidOrderPrice ? '✔️' : '❌') : ''}</span>
+						<span className='display-error-order'> {wrongOrderPrice ? notValidOrderPrice : ''}</span>
+					</div>
 
 					<input
 						placeholder="Bild URL"
@@ -84,8 +112,8 @@ function AddFood() {
 
 					<button type="submit">Lägg till mat</button>
 				</form>
-			</div>
-		</section>
+			</div >
+		</section >
 	)
 }
 
--- src/components/validation.js
+++ src/components/validation.js
@@ -1,34 +1,69 @@
-function isValid(userName) {
-	if (userName.length < 2) {
-		return [false, 'Behöver minst vara 2 tecken.']
-	}
-	const validChar = 'abcdefghijklmnopqrstuvwxyzåäö'
-	for (let i = 0; i < userName.length; i++) {
-		let character = userName.charAt(i).toLowerCase()
-		if (!validChar.includes(character)) {
+const validCharLetter = 'abcdefghijklmnopqrstuvwxyzåäö '
+const validCharLetterNumber = 'abcdefghijklmnopqrstuvwxyzåäö1234567890'
+const validCharComma = 'abcdefghijklmnopqrstuvwxyzåäö1234567890, '
+
+function isValid(validName) {
+	for (let i = 0; i < validName.length; i++) {
+		let character = validName.charAt(i).toLowerCase()
+		if (!validCharLetter.includes(character)) {
 			return [false, 'Vänligen använd endast bokstäver.']
 		}
+	}
+	if (validName.length < 2) {
+		return [false, 'Behöver minst vara 2 tecken.']
 	}
 	return [true, '']
 }
 
 function isValidPassword(userPassword) {
-	if (userPassword.length < 4) {
-		return [false, 'Behöver minst vara 4 tecken']
-	}
-	const validCharPass = 'abcdefghijklmnopqrstuvwxyzåäö1234567890'
 	for (let x = 0; x < userPassword.length; x++) {
 		let characterPassword = userPassword.charAt(x).toLowerCase()
-		if (!validCharPass.includes(characterPassword)) {
+		if (!validCharLetterNumber.includes(characterPassword)) {
 			return [false, 'Vänligen använd endast bokstäver och siffror.']
 		}
 	}
+	if (userPassword.length < 4) {
+		return [false, 'Behöver minst vara 4 tecken.']
+	}
 	if (userPassword != 'mums') {
-		return [false, 'Fel lösenord. Försök igen']
+		return [false, 'Fel lösenord. Försök igen.']
 	}
 	else {
 		return [true, '']
 	}
 }
 
-export { isValid, isValidPassword }
+function isValidIngredient(orderIngredient) {
+	for (let i = 0; i < orderIngredient.length; i++) {
+		let character = orderIngredient.charAt(i).toLowerCase()
+		if (!validCharComma.includes(character)){
+			return [false, 'Vänligen använd endast bokstäver, siffror eller komma.']
+		}
+	}
+	let ingredientInput = orderIngredient
+	let regex = /[^,]\s/
+	let hasIncorrectSpace = regex.test(ingredientInput)
+	if (hasIncorrectSpace == true) {
+		return [false, 'Vänligen använd komma innan mellanrum.']
+	}
+	return [true, '']
+}
+
+function isValidPrice(orderPrice) {
+	for (let x = 0; x < orderPrice.length; x++) {
+		let characterPassword = orderPrice.charAt(x).toLowerCase()
+		if (!validCharLetterNumber.includes(characterPassword)) {
+			return [false, 'Vänligen använd endast bokstäver och siffror.']
+		}
+	}
+	if (orderPrice.length < 5) {
+		return [false, 'Behöver minst vara 5 tecken']
+	}
+	let endsWithKr = orderPrice.endsWith('kr')
+	if (!endsWithKr) {
+		return [false, 'Måste avsluta med kr.']
+	}
+	return [true, '']
+}
+
+export { isValid, isValidPassword, isValidIngredient, isValidPrice }
