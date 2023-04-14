910d00634ead4df6eade9c32c188c710677b3576
NorthPew <vbergstrom02@gmail.com>
2023-04-14 09:39:02 +0200

    Added images for drinks

--- src/components/drinkMenuArray.js
+++ src/components/drinkMenuArray.js
@@ -2,22 +2,27 @@ const drinkMenuArray = [
 	{
 		name: "Coca-cola",
 		price: "35kr",
+		img: "./src/images/coke.jpeg"
 	},
 	{
 		name: "Fanta",
 		price: "35kr",
+		img: "./src/images/fanta.jpeg"
 	},
 	{
 		name: "Sprite",
 		price: "35kr",
+		img: "./src/images/sprite.jpeg"
 	},
 	{
 		name: "Lingondricka",
 		price: "10kr",
+		img: "./src/images/lingondryck.jpeg"
 	},
 	{
 		name: "Festis",
 		price: "15kr",
+		img: "./src/images/festis.jpeg"
 	},
 ]
 
