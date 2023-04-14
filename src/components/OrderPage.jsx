f5f517624bd04448f0e63572785ea383370fc02b
Forslunddavid <forslund.david@gmail.com>
2023-04-14 13:44:26 +0200

    löst problem med att ta bort maträtter med samma namn från b eställning

--- src/components/OrderPage.jsx
+++ src/components/OrderPage.jsx
@@ -1,58 +1,87 @@
 import { useState } from "react"
 
-function OrderPage({orderList, setOrderList}) {
+function OrderPage({ orderList, setOrderList }) {
+	const [completeOrder, setCompleteOrder] = useState("")
 
-    const [completeOrder, setCompleteOrder] = useState('')
+	const noSelectedFoodInBasket = (
+		<section className="order-error">
+			<span className="material-symbols-outlined">problem</span>
+			<h1>Du har inget i beställningar!</h1>
+		</section>
+	)
 
-    const noSelectedFoodInBasket = <section className="order-error"><span className="material-symbols-outlined">problem</span><h1>Du har inget i beställningar!</h1></section>
+	const deleteDish = (index) => {
+		let copy = orderList.filter((dish, i) => i !== index)
+		setOrderList(copy)
+		console.log("Copy", copy, "orderList", orderList)
+	}
 
+	const orderCompleteTrigger = () => {
+		setCompleteOrder(
+			`Du har nu slutfört din beställning. Ditt ordernummer: #  ${Math.floor(
+				Math.random() * 100
+			)}`
+		)
+	}
 
-    const deleteDish = (orderName) => {
-        let copy = orderList.filter(dish => (dish.name !== orderName))
-        setOrderList(copy)
-        console.log('Copy', copy, 'orderList', orderList );
-    }
-
-    const orderCompleteTrigger = () => {
-      setCompleteOrder(`Du har nu slutfört din beställning. Ditt ordernummer: #  ${Math.floor(Math.random() * 100)}`)
-    }
-
-    return (
-        <div className="order-page">
-            <h1 className="order-heading">Beställningar</h1>
-            {orderList.length == 0 ? "" : <section className="order-total">
-                    <span>Totala priset: {orderList.reduce((delsumma, order) => delsumma + Number(order.price.replace('kr', '')), 0)} kr
-                    </span>
-                </section>}
-            <ul className="order-list">
-                {orderList.length == 0 ? noSelectedFoodInBasket : orderList.map(order  => (
-                    <>
-                    <li className="order-box" key={order.name}>
-                            <div className="order-container">
-                                <h2 className="order-name">{order.name}</h2>
-                                <img
-                                    className="order-img"
-                                    src={order.img}
-                                    alt={order.name}
-                                />
-                                <p className="order-price">{order.price}</p>
-                            </div>
-                            <p className="order-ingredients">{order.ingredients}</p>
-                            <div className="food-button-bar">
-                                <button onClick={() => deleteDish(order.name)}><span className="material-symbols-outlined">cancel</span></button>
-                            </div>
-                        </li>
-                    </> 
-                ))}
-            </ul>
-            {orderList.length == 0 ? "" : <section className="order-complete">
-                    <button onClick={orderCompleteTrigger}>Slutför beställning
-                    </button>
-                    <p>{completeOrder}</p>
-                    </section>}
-        </div>
-
-    )
+	return (
+		<div className="order-page">
+			<h1 className="order-heading">Beställningar</h1>
+			{orderList.length == 0 ? (
+				""
+			) : (
+				<section className="order-total">
+					<span>
+						Totala priset:{" "}
+						{orderList.reduce(
+							(delsumma, order) =>
+								delsumma +
+								Number(order.price.replace("kr", "")),
+							0
+						)}{" "}
+						kr
+					</span>
+				</section>
+			)}
+			<ul className="order-list">
+				{orderList.length == 0
+					? noSelectedFoodInBasket
+					: orderList.map((order, index) => (
+							<li className="order-box" key={index}>
+								<div className="order-container">
+									<h2 className="order-name">{order.name}</h2>
+									<img
+										className="order-img"
+										src={order.img}
+										alt={order.name}
+									/>
+									<p className="order-price">{order.price}</p>
+								</div>
+								<p className="order-ingredients">
+									{order.ingredients}
+								</p>
+								<div className="food-button-bar">
+									<button onClick={() => deleteDish(index)}>
+										<span className="material-symbols-outlined">
+											cancel
+										</span>
+									</button>
+								</div>
+							</li>
+					  ))}
+			</ul>
+			{orderList.length == 0 ? (
+				""
+			) : (
+				<section className="order-complete">
+					<button onClick={orderCompleteTrigger}>
+						Slutför beställning
+					</button>
+					<p>{completeOrder}</p>
+				</section>
+			)}
+		</div>
+	)
 }
 
 export default OrderPage
