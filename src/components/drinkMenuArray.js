698951af33022382feda426334da724866f4e920
NorthPew <vbergstrom02@gmail.com>
2023-04-18 11:22:16 +0200

    Something happened but fixed now

--- src/components/drinkMenuArray.js
+++ src/components/drinkMenuArray.js
@@ -2,47 +2,27 @@ const drinkMenuArray = [
 	{
 		name: "Coca-cola",
 		price: "35kr",
-<<<<<<< HEAD
 		id: 6,
-=======
-		id: 6
->>>>>>> complete-order
 	},
 	{
 		name: "Fanta",
 		price: "35kr",
-<<<<<<< HEAD
 		id: 7,
-=======
-		id: 7
->>>>>>> complete-order
 	},
 	{
 		name: "Sprite",
 		price: "35kr",
-<<<<<<< HEAD
 		id: 8,
-=======
-		id: 8
->>>>>>> complete-order
 	},
 	{
 		name: "Lingondricka",
 		price: "10kr",
-<<<<<<< HEAD
 		id: 9,
-=======
-		id: 9
->>>>>>> complete-order
 	},
 	{
 		name: "Festis",
 		price: "15kr",
-<<<<<<< HEAD
 		id: 10,
-=======
-		id: 10
->>>>>>> complete-order
 	},
 ]
 
