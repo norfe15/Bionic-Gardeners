7640c492e85959428a47af608fe1aa7c409ea638
Forslunddavid <forslund.david@gmail.com>
2023-04-18 09:34:20 +0200

    fixat key occh rensat onödig kod

--- src/components/OrderPageButton.jsx
+++ src/components/OrderPageButton.jsx
@@ -1,22 +1,17 @@
 function OrderPageButton({ setOrderList, item, drink }) {
 	const testFunction = () => {
 		setOrderList((orderList) => {
-            if (item) {
-			// Gör kopia av orderList
-			// Lägg till item till nya orderList
-			// Retunera nya orderList
-
-			let copiedList = [...orderList]
-			copiedList.push(item)
-			console.log(copiedList)
-			return copiedList
-            } else if (drink) {
-			let copiedList = [...orderList]
-			copiedList.push(drink)
-			console.log(copiedList)
-			return copiedList 
-            }
-
+			if (item) {
+				let copiedList = [...orderList]
+				copiedList.push(item)
+				console.log(copiedList)
+				return copiedList
+			} else if (drink) {
+				let copiedList = [...orderList]
+				copiedList.push(drink)
+				console.log(copiedList)
+				return copiedList
+			}
 		})
 	}
 
